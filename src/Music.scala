834e943241dde480bf10b7c533ce93d640dac1b1
SoPlays <joao.ranger@gmail.com>
2020-11-10 17:15:14 +0000

    1st commit

---
+++ src/Music.scala
@@ -0,0 +1,20 @@
+case class Music (nome:String, duration:Double, artist:String, genre:String, album:String){
+
+
+
+}
+
+object Music{
+
+  type Nome       = String
+  type Duration   = Double
+  type Artist     = String
+  type Genre      = String
+  type Album      = String
+
+  def  ():{
+
+
+  }
+
+}
