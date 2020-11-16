af402ee22739347f7fd6775c030a2507c98a7a2f
AfonsoCostaVale <afonsocostavale@gmail.com>
2020-11-16 15:55:22 +0000

    BD e remove playlist

--- src/Data/Song.scala
+++ src/Data/Song.scala
@@ -1,8 +1,8 @@
 package Data
 
-case class Song ( filepath:String, name:String, duration:Double, artist:String, genre:String, album:String, feats: List[String], listened:Int){
+case class Song ( filepath:String, name:String, duration:Int, artist:String, genre:String, album:String, feats: List[String], listened:Int){
 
-  def info(): Option[(String,String,Double,String,String,String,List[String],Int)] ={ Song.info(this) }
+  def info(): Option[(String,String,Int,String,String,String,List[String],Int)] ={ Song.info(this) }
 
   override def toString(): String ={ Song.toString(this) }
 
@@ -11,7 +11,7 @@ object Song{
 
   type Filepath   = String
   type Name       = String
-  type Duration   = Double
+  type Duration   = Int
   type Artist     = String
   type Genre      = String
   type Album      = String
@@ -19,7 +19,7 @@ object Song{
   type Listened   = Int
 
 
-  def info(s:Song): Option[(String,String,Double,String,String,String,List[String],Int)] ={
+  def info(s:Song): Option[(String,String,Int,String,String,String,List[String],Int)] ={
     Option(
       s.filepath,
       s.name,
@@ -31,9 +31,14 @@ object Song{
       s.listened
     )
   }
-
-  def toString(s:Song ): String ={
-    s.filepath + ";" + s.name + ";" + s.duration + ";" + s.artist + ";" + s.genre + ";" + s.album + ";" + s.feats + ";" + s.listened
+//---------  LOAD APPLY
+  def apply(filepath:String, name:String, duration:Int, artist:String, genre:String, album:String, feats: String, listened:Int): Song = {
+    Song(filepath,name,duration,artist,genre,album,feats.split(" ").toList,listened)
   }
+//----------
+  def toString(s:Song ): String ={
+    s.filepath + ";" + s.name + ";" + s.duration + ";" + s.artist + ";" + s.genre + ";" + s.album + ";" + s.feats.mkString(" ") + ";" + s.listened +";end;"
+  }
+
 
 }
--- src/Main.scala
+++ src/Main.scala
@@ -1,17 +1,24 @@
+import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
+
 import Data._
 
 import scala.io.StdIn.readLine
-
+import scala.io.Source
+import scala.collection.mutable.ListBuffer
 
 object Main {
-  def  main (args: Array[String]): Unit = {
-    var album:Album = Album("album1")
-    var artist:Artist = Artist("artist1","album1")
-    var s:Song = Song("path","songname",100,"artist1","genre","album1",Nil,10)
-    println(s.info())
-    println(s.toString())
+  var loadedSongs = new ListBuffer[Song]()
+  var loadedArtists = new ListBuffer[Artist]()
+  var loadedAlbums =  new ListBuffer[Album]()
+  var loadedPlaylists = new ListBuffer[Playlist]()
+  val db_songs="DataBases/db_songs"
+  val db_artists="DataBases/db_artists"
+  val db_albums="DataBases/db_albums"
+  val db_playlists="DataBases/db_playlists"
 
-    //  mainLoop()
+  def  main (args: Array[String]): Unit = {
+    loadfiles()
+    printLoaded()
   }
 
   def showPropt(): Unit ={
@@ -21,13 +28,76 @@ object Main {
   def getUserInput(): String ={
       readLine.trim.toUpperCase
   }
+  def loadfiles(): Unit={
+    loadedSongs = new ListBuffer[Song]()
+    loadedArtists = new ListBuffer[Artist]()
+    loadedAlbums =  new ListBuffer[Album]()
+    loadedPlaylists = new ListBuffer[Playlist]()
+    readFile(loadSong,db_songs)
+    readFile(loadArtist,db_artists)
+    readFile(loadAlbum,db_albums)
+    readFile(loadPlaylist,db_playlists)
+  }
+  def loadSong(line: String): Unit={
+    val info=line.split(";").toList
+    loadedSongs+=Song(info(0),info(1),info(2).toInt,info(3),info(4),info(5),info(6),info(7).toInt)
+    println("Song loaded from DB")
+  }
+  def loadArtist(line: String): Unit={
+    val info=line.split(";").toList
+    loadedArtists+=Artist(info(0),info(1),info(2))
+    println("Artist loaded from DB")
+  }
+  def loadAlbum(line: String): Unit={
+    val info=line.split(";").toList
+    loadedAlbums+=Album(info(0),info(1),info(2))
+    println("Album loaded from DB")
+  }
+  def loadPlaylist(line: String): Unit={
+    val info=line.split(";").toList
+    loadedPlaylists+=Playlist(info(0),info(1),info(2))
+    println("Playlist loaded from DB")
+  }
+  def readFile(load: String=>Unit, filename: String): Unit={
+    val bufferedFile = Source.fromFile(filename)
+    val lines = bufferedFile.getLines.toList
+    readline(load,lines)
+    bufferedFile.close()
+
+  }
+  def readline(load: String=>Unit, lines: List[String]): Unit= lines match{
+    case a::Nil => load(a)
+    case a::t => load(a);readline(load,t)
+  }
+  def addtoDB[A](a: A, dbPath: String):Unit={
+    var file=new File(dbPath)
+    val bw = new BufferedWriter(new FileWriter(file, true))
+    bw.newLine()
+    bw.write(a.toString())
+    bw.close()
+  }
+  def deletefromDB[A](a: A, dbPath: String):Unit={
+    val bufferedFile = Source.fromFile(dbPath)
+    val lines = bufferedFile.getLines.toList.filter(_ != a.toString)
+    bufferedFile.close()
+    var file=new File(dbPath)
+    val bw = new BufferedWriter(new FileWriter(file))
+    writeFileAfterDelete(bw,lines)
+    bw.close()
+  }
+  def writeFileAfterDelete(bw: BufferedWriter, lines: List[String]): Unit = lines match{
+    case a::Nil => bw.write(a);bw.newLine();bw.close()
+    case a::t => bw.write(a);bw.newLine();writeFileAfterDelete(bw,t)
+  }
+  def printLoaded(): Unit={
+    loadedSongs.foreach{println}
+    loadedArtists.foreach{println}
+    loadedAlbums.foreach{println}
+    loadedPlaylists.foreach{println}
+  }
 
   /*
   Lista de funções
-      WRITE TO FILESSSS
-      delete from files
-      read from files
-
       sacar info da musica a partir do filepath(1ª criação da musica / importar musica para o sistema)
 
   */
