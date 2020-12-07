39775b6f832659e848913dab3bd394b59d989d29
SoPlays <joao.ranger@gmail.com>
2020-12-07 03:15:40 +0000

    Much advance much to do - afons

--- src/Main.scala
+++ src/Main.scala
@@ -29,19 +29,6 @@ class Main extends Application{
   override def start(primaryStage: Stage): Unit = {
 
     primaryStage.setTitle("MP3")
-    /*
-    val file:File = new File("C:/Joao/Music/Arctic Monkeys/AM (Album)/test.mp3")
-    val fileChooser = new FileChooser
-    val selectedFile:File = fileChooser.showOpenDialog(primaryStage)
-
-    //val resource = getClass.getClassLoader.getResource("C:/Joao/Music/doiwannaknow.mp3")
-    println(selectedFile.getAbsoluteFile)
-    val resource = getClass.getClassLoader.getResource(selectedFile.getAbsolutePath)
-    //val pick: Media = new Media(resource.toString)
-    val pick: Media = new Media(selectedFile.toURI.toString)
-    val player: MediaPlayer = new MediaPlayer(pick)
-    val m: MediaView = new MediaView(player)
-    */
 
     val fxmlLoader = new FXMLLoader(getClass.getResource("Controller.fxml"))
 
