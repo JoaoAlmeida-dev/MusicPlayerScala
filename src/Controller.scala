e539dfc5a66ce7dd0917ce419e8895db608bf2c4
SoPlays <joao.ranger@gmail.com>
2020-12-07 23:29:45 +0000

    all ok to play from loaded

--- src/Controller.scala
+++ src/Controller.scala
@@ -11,7 +11,7 @@ import javafx.util.Duration
 
 import java.io.File
 import scala.collection.mutable.ListBuffer
-import scala.util.Success
+import scala.util.{Failure, Success, Try}
 
 
 
@@ -36,50 +36,145 @@ class Controller {
   @FXML private var leftPane: AnchorPane = _
   @FXML private var songList: ListView[Song] = new ListView[Song]()
 
-  var mediaControl: MediaController = MediaController()
+  var mediaPlayer: MediaPlayer = _
 
   def initialize(): Unit ={
     DatabaseFunc.loadfiles()
     Song.loaded.map(songList.getItems.add)
+    val firstSongPath:String=Song.loaded(0).filepath
+    mediaPlayer = new MediaPlayer(new Media(new File(firstSongPath).toURI.toString))
+  }
+
+  def importMusic(): Unit = {
+    val stage: Stage = (chooseFileButton.getScene.getWindow).asInstanceOf[Stage]
+    val fileChooser = new FileChooser
+    val selectedFile:File = fileChooser.showOpenDialog(stage)
+    uploadSong(selectedFile)
+
 
   }
 
-  def chooseFile(): Unit = {
-    val stage: Stage = (chooseFileButton.getScene.getWindow).asInstanceOf[Stage]
-    val fileChooser = new FileChooser
-    val selectedFile = fileChooser.showOpenDialog(stage)
-    //val pick = new Media(selectedFile.toURI.toString)
-    //val mc=MediaController(selectedFile.getAbsolutePath)
-    mediaControl = MediaController(selectedFile.getAbsolutePath)
-    val info:ListBuffer[(String,AnyRef)]=ListBuffer[(String,AnyRef)]()
+  def setSeekSlider(): Unit = {
+    //minDurationLabel.setText(math.round(seektime).toString)
+    val time:(Int,Int,Int)= msToMinSec(mediaPlayer.getTotalDuration())
+    maxDurationLabel.setText(time._2+":"+time._3)
+    minDurationLabel.setText("0:0")
+  }
 
-    musicNameLabel.setText(selectedFile.getName)
+  def currentTimeLabelSet(time: String): Unit = {
+    minDurationLabel.setText(time)
+  }
 
-    mediaControl.mediaPlayer.get.currentTimeProperty().addListener(new ChangeListener[Duration] {
+  def setDurationSlider(value: Double): Unit = {
+    durationSlider.setValue(value)
+  }
+
+
+
+  def dragDuration(): Unit = {
+    this.synchronized{
+      val seektime: Double = ((durationSlider.getValue * mediaPlayer.getTotalDuration().toMillis) / 100)
+      mediaPlayer.seek(new Duration(seektime))
+    }
+  }
+
+  def setVolume(): Unit = {
+    val volume: Double = volumeSlider.getValue
+    mediaPlayer.setVolume(volume / 100)
+    volumeLabel.setText(volume.toInt.toString + "%")
+  }
+
+
+
+  private def msToMinSec(duration: Duration):(Int,Int,Int)={
+    val hours:Int   = math.floor(duration.toHours).toInt
+    val minutes:Int = math.floor(duration.toMinutes).toInt-(hours*60)
+    val sec:Int     = math.floor(duration.toSeconds).toInt-(hours*60*60)-(minutes*60)
+
+    (hours,minutes,sec)
+  }
+
+  //MediaPlayer Functions
+  def playpause(): Unit = {
+    if (!mediaPlayer.getStatus.equals(MediaPlayer.Status.PLAYING)) {
+      togglePlayPause.setText("Pause")
+      mediaPlayer.play()
+    } else {
+      togglePlayPause.setText("Play")
+      mediaPlayer.pause()
+    }
+
+  }
+
+  def setListeners(): Unit ={
+    //progress Slider updater
+    mediaPlayer.currentTimeProperty().addListener(new ChangeListener[Duration] {
       override def changed(observable: ObservableValue[_ <: Duration], oldValue: Duration, newValue: Duration): Unit = {
         val time:(Int,Int,Int)= msToMinSec(newValue)
         currentTimeLabelSet(time._2+":"+time._3)
-        setDurationSlider((newValue.toSeconds * 100) / mediaControl.getMaxDuration().toSeconds)
+        setDurationSlider((newValue.toSeconds * 100) / mediaPlayer.getTotalDuration.toSeconds)
       }
     })
+  }
 
-    mediaControl.media.get.getMetadata().addListener(new MapChangeListener[String , AnyRef]{
+  def selectFromList(): Unit ={
+    val song:Song = songList.getSelectionModel.getSelectedItems.get(0)
+    mediaChange(song.filepath)
+    musicNameLabel.setText(song.name)
+  }
+
+  def resetPlayButton(): Unit ={
+    togglePlayPause.setSelected(false)
+    togglePlayPause.setText("Play")
+  }
+
+  def mediaChange(filepath:String): Unit ={
+
+    val media :Try[Media]= Try(new Media(new File(filepath).toURI.toString))
+    media match{
+      case Success(v) => {
+        val volume = mediaPlayer.getVolume
+        mediaPlayer.dispose()
+        mediaPlayer = new MediaPlayer(v)
+        mediaPlayer.setVolume(volume)
+        setListeners()
+        resetPlayButton()
+      }
+      case Failure(e) => {
+        print("Erro a criar media")
+        throw e
+      }
+
+    }
+
+  }
+
+
+
+
+
+
+
+
+
+  private def uploadSong(selectedFile: File): Unit={
+    val media = new Media(selectedFile.toURI.toString)
+    val metadataMediaPlayer = new MediaPlayer(media)
+    val info:ListBuffer[(String,AnyRef)]=ListBuffer[(String,AnyRef)]()
+    media.getMetadata().addListener(new MapChangeListener[String , AnyRef]{
       override def onChanged(change: MapChangeListener.Change[_ <: String, _ <: AnyRef]): Unit ={
         if(change.wasAdded()){
           info.addOne((change.getKey() , change.getValueAdded))
         }
-       }
+      }
     })
-
-    mediaControl.mediaPlayer.get.setOnReady(new Runnable {
+    metadataMediaPlayer.setOnReady(new Runnable {
       override def run(): Unit = {
-        setSeekSlider()
-        println(info)
 
         val album:String = info.filter( x=>x._1.equals("album") ) (0)._2.toString
         val artist:String = info.filter( x=>x._1.equals("album artist") )(0)._2.toString
         val songid = DatabaseFunc.getlastid(Song.loaded.toList)+1
-//artist exists? if not creating it
+        //artist exists? if not creating it
         val artistcheck:ListBuffer[Artist]=Artist.loaded.filter(x=>x.name.equals(artist))
 
         val artistid:Int={
@@ -92,7 +187,7 @@ class Controller {
           }
         }
 
-//album exists? if not creating it
+        //album exists? if not creating it
         val albumcheck:ListBuffer[Album]=Album.loaded.filter(x=>x.name.equals(album))
 
         val albumid:Int = {
@@ -108,6 +203,12 @@ class Controller {
         nomeFeats.map(println)
         val idFeats=nomeFeats.map(x=>DatabaseFunc.GetIDArtistOrCreate(x))
 
+        val trackNaux:ListBuffer[(String,AnyRef)]=info.filter(x => x._1.equals("track number"))
+        val trackN = if(trackNaux.isEmpty){
+          0
+        }else{
+          trackNaux(0)._2.toString.trim
+        }
         val song:Song = Song(List[String](
           songid.toString,//0
           info.filter( x=>x._1.equals("title") )(0)._2.toString.trim,//1
@@ -118,87 +219,12 @@ class Controller {
           idFeats.mkString(" "),//7 feats
           0.toString,//8
           trackN.toString,//9 TrackNumber resolver
-          )
+        )
         )
         println(song)
         Song.loaded+=song
         //songList.getItems.add(song)
       }
     })
-
   }
-
-  def setSeekSlider(): Unit = {
-    //minDurationLabel.setText(math.round(seektime).toString)
-    val time:(Int,Int,Int)= msToMinSec(mediaControl.getMaxDuration())
-    maxDurationLabel.setText(time._2+":"+time._3)
-    minDurationLabel.setText("0:0")
-  }
-
-  def currentTimeLabelSet(time: String): Unit = {
-    minDurationLabel.setText(time)
-  }
-
-  def setDurationSlider(value: Double): Unit = {
-    durationSlider.setValue(value)
-  }
-
-  def playpause(): Unit = {
-    mediaControl.playpause()
-    if (mediaControl.mediaPlayer.get.getStatus.equals(MediaPlayer.Status.PLAYING)) {
-      togglePlayPause.setText("Play")
-    } else {
-      togglePlayPause.setText("Pause")
-    }
-
-  }
-
-  def dragDuration(): Unit = {
-    this.synchronized{
-      val seektime: Double = ((durationSlider.getValue * mediaControl.getMaxDuration().toMillis) / 100)
-      mediaControl.seek(new Duration(seektime))
-    }
-  }
-
-  def setVolume(): Unit = {
-    val volume: Double = volumeSlider.getValue
-    mediaControl.setVolume(volume / 100)
-    volumeLabel.setText(volume.toInt.toString + "%")
-  }
-
-  def selectFromList(): Unit ={
-    val song:Song = songList.getSelectionModel.getSelectedItems.get(0)
-    if(mediaControl.path == ""){
-    mediaControl.mediaPlayer.get.dispose()
-    }
-
-    mediaControl = MediaController(song.filepath)
-    musicNameLabel.setText(song.name)
-    val info:ListBuffer[(String,AnyRef)]=ListBuffer[(String,AnyRef)]()
-    mediaControl.mediaPlayer.get.currentTimeProperty().addListener(new ChangeListener[Duration] {
-      override def changed(observable: ObservableValue[_ <: Duration], oldValue: Duration, newValue: Duration): Unit = {
-        val time:(Int,Int,Int)= msToMinSec(newValue)
-        currentTimeLabelSet(time._2+":"+time._3)
-        setDurationSlider((newValue.toSeconds * 100) / mediaControl.getMaxDuration().toSeconds)
-      }
-    })
-
-    mediaControl.media.get.getMetadata().addListener(new MapChangeListener[String , AnyRef]{
-      override def onChanged(change: MapChangeListener.Change[_ <: String, _ <: AnyRef]): Unit ={
-        if(change.wasAdded()){
-          info.addOne((change.getKey() , change.getValueAdded))
-        }
-      }
-    })
-  }
-
-  private def msToMinSec(duration: Duration):(Int,Int,Int)={
-    val hours:Int   = math.floor(duration.toHours).toInt
-    val minutes:Int = math.floor(duration.toMinutes).toInt-(hours*60)
-    val sec:Int     = math.floor(duration.toSeconds).toInt-(hours*60*60)-(minutes*60)
-
-    (hours,minutes,sec)
-  }
-
-
 }
